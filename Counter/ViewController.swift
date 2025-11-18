dadce29d81b91cb6af2da8c12ed1a22949073119
Ekaterina <bardanovaekaterina7@gmail.com>
2025-11-18 18:56:27 +0100

    Added project Counter with a counter and a history of changes

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var count: UILabel!
+    
+    @IBOutlet weak var countButton: UIButton!
+    
+    @IBOutlet weak var minusButton: UIButton!
+    
+    @IBOutlet weak var zeroButton: UIButton!
+    
+    @IBOutlet weak var historyOfChanges: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    var countValue: Int = 0
+    var historyText: String = ""
+    
+    func currentDateTime() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        return formatter.string(from: Date())
+    }
+    
+    @IBAction func plusButtonTouched(_ sender: Any) {
+        countValue += 1
+        count.text = "Значение счетчика:\(countValue)"
+        
+        
+        let stringHistory = "\(currentDateTime()): значение изменено на +1\n"
+        historyText += stringHistory
+        historyOfChanges.text = historyText
+        
+    }
+        
+    @IBAction func minusButtonTouched(_ sender: Any) {
+        if countValue > 0 {
+            countValue -= 1
+            count.text = "Значение счетчика:\(countValue)"
+            
+            let stringHistory = "\(currentDateTime()): значение изменено на -1\n"
+            historyText += stringHistory
+            historyOfChanges.text = historyText
+        } else {
+            let stringHistory = "\(currentDateTime()): попытка уменьшить значение счётчика ниже 0\n"
+            historyText += stringHistory
+            historyOfChanges.text = historyText
+        }
+        
+    }
+    
+    @IBAction func zeroButtonTouched(_ sender: Any) {
+        countValue = 0
+        count.text = "Значение счетчика:\(countValue)"
+        
+        let stringHistory = "\(currentDateTime()): значение сброшено\n"
+        historyText += stringHistory
+        historyOfChanges.text = historyText
+    }
 }
+    
 
